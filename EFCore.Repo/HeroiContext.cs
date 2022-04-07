af8cb824ac07fa2cbec0216d7f2f84032898ae5a
duribeiro <eduardo.duduribeiro1@gmail.com>
2022-04-07 19:26:36 -0400

    Move o contexto para a camada repositorio

--- EFCore.Repo/HeroiContext.cs
+++ EFCore.Repo/HeroiContext.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EFCore.WebApi.Data
+namespace EFCore.Repo
 {
     public class HeroiContext : DbContext
     {
